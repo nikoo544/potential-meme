b42eeeb095a32379b944f86be41e2d9c21d79861
Sam Ray <s4muraixh@gmail.com>
2022-01-27 19:18:21 -0300

    Initial commit

---
+++ data/authors/default.md
@@ -0,0 +1,15 @@
+---
+name: Nico Escudero
+avatar: /static/images/avatar.png
+occupation: Make a computer say beep-boop
+company: Berserker AR
+email: niko@berserker.ar
+twitter: https://twitter.com/nicoescudero
+linkedin: https://www.linkedin.com/nico-escudero
+github: https://github.com/nikoo544
+---
+
+
+
+**Bjarkan er laufgrønstr líma;
+Loki bar flærða tíma.**
---
+++ data/projectsData.ts
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Dev Blog',
+    description: `Blog para compartir mis proyectos y experiencias`,
+    imgSrc: '/static/images/berserkblog.png',
+    href: '/blog',
+  },
+  {
+    title: 'Art blog',
+    description: `Personal website for my drawings.`,
+    imgSrc: '/static/images/drawing.png',
+    href: 'https://gloriusexecutioner.tumblr.com/',
+  },
+]
+
+export default projectsData
